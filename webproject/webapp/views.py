5391540423528e45c2f9b5dee39afa4f5412760d
Piwero <pinero@Joses-MacBook-Air.local>
2021-02-04 15:32:52 +0000

    show object attributes in services

--- webproject/webapp/views.py
+++ webproject/webapp/views.py
@@ -1,20 +1,27 @@
 from django.shortcuts import render, HttpResponse
 
+from services.models import Service
+
 
 # Create your views here.
 def home(request):
+
     return render(request, 'webapp/home.html')
 
 
 def services(request):
-    return render(request, 'webapp/services.html')
+    services = Service.objects.all()
+
+    return render(request, 'webapp/services.html', {"services": services})
 
 
 def shop(request):
+
     return render(request, 'webapp/shop.html')
 
 
 def blog(request):
+
     return render(request, 'webapp/blog.html')
 
 
