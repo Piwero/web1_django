996adc2460cd327ca20718b362f4e783c650a6f6
Piwero <pinero@Joses-MacBook-Air.local>
2021-02-04 10:15:05 +0000

    first model for services with meta and str

--- webproject/services/models.py
+++ webproject/services/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+class Service(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.CharField(max_length=50)
+    image = models.ImageField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'service'
+        verbose_name_plural = 'services'
+
+    def __str__(self):
+        return self.title
