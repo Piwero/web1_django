68db47b3c041aef6a52c29355d7bffbb63dd2bdc
Piwero <pinero@Joses-MacBook-Air.local>
2021-02-05 16:32:51 +0000

    change url and view to services app

--- webproject/webapp/urls.py
+++ webproject/webapp/urls.py
@@ -5,7 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('services/', views.services, name='services'),
     path('shop/', views.shop, name='shop'),
     path('blog/', views.blog, name='blog'),
     path('contact/', views.contact, name='contact'),
