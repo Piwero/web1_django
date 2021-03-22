2bd03ade386ff6689c5d21d9fc194d5a7a01c56b
Piwero <piwero@gmail.com>
2021-03-22 15:44:44 +0000

    create product and category models and migration

--- webproject/shop/models.py
+++ webproject/shop/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=50)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "ProductCategory"
+        verbose_name_plural = "ProductCategories"
+
+        def __str__(self):
+            return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    categories = models.ForeignKey(ProductCategory, on_delete= models.CASCADE)
+    image = models.ImageField(upload_to="shop", null=True, blank=True)
+    price = models.FloatField()
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
+
+        def __str__(self):
+            return self.name
+
+
+
