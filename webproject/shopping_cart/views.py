c90314b3d146b7a45b4cfb8a05b2fb0439ad7e7d
Piwero <piwero@gmail.com>
2021-05-20 10:29:30 +0100

    create views shoppingcart

--- webproject/shopping_cart/views.py
+++ webproject/shopping_cart/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 
-# Create your views here.
+from webproject.shop.models import Product
+from webproject.shopping_cart.shoppingcart import ShoppingCart
+
+
+def add_product(request, product_id):
+    shopping_cart =ShoppingCart(request)
+    product = Product(id= product_id)
+    shopping_cart.add_product(product)
+
+    return redirect("shop")
+
+
+def eliminate_product(request, product_id):
+    shopping_cart = ShoppingCart(request)
+    product = Product(id=product_id)
+    shopping_cart.eliminate(product)
+
+    return redirect("shop")
+
+
+def reduce_product(request, product_id):
+    shopping_cart = ShoppingCart(request)
+    product = Product(id=product_id)
+    shopping_cart.reduce_product(product)
+
+    return redirect("shop")
