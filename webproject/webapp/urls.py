28e02e7cd3ed6b666952433f34e272ac76e2cdbb
Piwero <piwero@gmail.com>
2021-06-04 09:37:20 +0100

    change import models webapp urls

--- webproject/webapp/urls.py
+++ webproject/webapp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-
 # from . import views
-from webapp import views
+from webproject.webapp import views
+
+from . import views
+>>>>>>> change import models webapp urls
 from django.conf import settings
 from django.conf.urls.static import static
 
