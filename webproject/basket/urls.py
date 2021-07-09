d252845ad81aa39ae3135a53d5c5327ac97de0c1
Piwero <piwero@gmail.com>
2021-07-09 15:30:11 +0100

    fix widget basket

---
+++ webproject/basket/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name="basket"
+urlpatterns = [
+    path("add/<int:product_id>/", views.add_product, name="add"),
+    path("reduce/<int:product_id>/", views.reduce_product, name="reduce"),
+    path("eliminate/<int:product_id>/", views.eliminate_product, name="eliminate"),
+    path("reset/", views.reset_basket, name="reset"),
+
+]
+
