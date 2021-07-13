68dd3731e0f4a3d7d753d287aaca29ee78434921
Piwero <piwero@gmail.com>
2021-07-13 17:15:34 +0100

    modify admin to test pre-commit

--- webproject/services/admin.py
+++ webproject/services/admin.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from .models import Service
 
 
-# Register your models here.
-
-
 class ServiceAdmin(admin.ModelAdmin):
-    readonly_fields = ("created", "updated")
+
+    readonly_fields = (" created", "updated")
+
+
 
 
 admin.site.register(Service, ServiceAdmin)
