262a6f29ea1e9e49542f97fbee81048d97b66d6f
Piwero <piwero@gmail.com>
2021-07-14 21:22:18 +0100

    add secret key in base settings

--- webproject/webproject/settings/base.py
+++ webproject/webproject/settings/base.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = "8s1%5x$cw1@w&p+=%h5b^8&s7ay7vfrt(wp0*pk@40-as$g1)e"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
