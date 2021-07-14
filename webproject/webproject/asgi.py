098060a6cf70846623f3d4c484a13dda00405db0
Piwero <piwero@gmail.com>
2021-07-14 17:35:57 +0100

    modify asgi to import settings

--- webproject/webproject/asgi.py
+++ webproject/webproject/asgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webproject.settings.production")
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "webproject.webproject.settings.production"
+)
 
 application = get_asgi_application()
