465aed62638c32c1925a9a3c762045590650f772
Piwero <piwero@gmail.com>
2021-07-14 15:38:55 +0100

    add local settings to run from manage.py

--- webproject/manage.py
+++ webproject/manage.py
@@ -6,7 +6,10 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webproject.settings")
+    if os.path.isfile("db.sqlite3"):
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webproject.settings.local")
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webproject.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
