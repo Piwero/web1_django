4980dfb17d67ba8b09f77e01dd9aea82f0b55e02
Piwero <piwero@gmail.com>
2021-08-17 17:18:40 +0100

    update production

--- webproject/core/settings/production.py
+++ webproject/core/settings/production.py
@@ -3,11 +3,11 @@ import os
 from .base import *  # noqa
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-ALLOWED_HOSTS = [".herokuapp.com"]
+ALLOWED_HOSTS = ["web1-django.herokuapp.com"]
 
 SECURE_SSL_REDIRECT = True
 
 STATICFILES_DIRS += [
-    os.path.join(BASE_DIR, "web1_django/build/static"),
+    os.path.join(BASE_DIR, "webproject/build/static"),
     os.path.join(BASE_DIR, "build/"),
 ]
