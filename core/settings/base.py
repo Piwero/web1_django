14e8a23d23d86d2d634f41c35624b22a9c3fabbb
Piwero <piwero@gmail.com>
2021-08-18 15:20:38 +0100

    remove project name

--- core/settings/base.py
+++ core/settings/base.py
@@ -135,9 +135,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATICFILES_DIRS = os.path.join(BASE_DIR, "static")
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+
 STATICFILES_FINDER = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
