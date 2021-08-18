d80e17f377c9c8bc178a6bd573befb70ffefc2a0
Piwero <piwero@gmail.com>
2021-08-18 15:24:21 +0100

    add staticfiles in production staticfiles dirs

--- core/settings/production.py
+++ core/settings/production.py
@@ -9,6 +9,7 @@ SECURE_SSL_REDIRECT = True
 
 STATICFILES_DIRS += [
     os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "staticfiles"),
     os.path.join(BASE_DIR, "build/static"),
     os.path.join(BASE_DIR, "build/"),
 ]
