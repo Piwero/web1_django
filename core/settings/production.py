ba5fa24f26f352fafc80d0ecbe8217e5e5fb0ae2
Piwero <piwero@gmail.com>
2021-08-18 16:09:15 +0100

    add static files

--- core/settings/production.py
+++ core/settings/production.py
@@ -1,14 +1,6 @@
-import os
-
 from .base import *  # noqa
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 ALLOWED_HOSTS = ["piwero-django1.herokuapp.com"]
 
 SECURE_SSL_REDIRECT = True
-
-STATICFILES_DIRS += [
-    os.path.join(BASE_DIR, "static"),
-    os.path.join(BASE_DIR, "build/static"),
-    os.path.join(BASE_DIR, "build/"),
-]
